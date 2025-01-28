a35575bbec40917ba1b353d7d2d09220b86341a5
Vic428-human <z0983195379a@gmail.com>
2025-01-28 16:33:20 +0800

    git rm --cached -r .next/

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eWzzvC6V+6cB/Pb5ajvUdFDD/VoJudRkK+hoApjfSlU=\"\n}"
---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MyComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>
+        This is a functional component using an arrow function in React 18 with
+        TypeScript.
+      </p>
+    </div>
+  );
+};
+
+export default MyComponent;
